8a2d43d7f40e5ec27247a71154c42a497b276aa8
deokgoo <kkddgg1001@gmail.com>
2020-11-20 20:18:20 +0900

    feature: animation header & footer

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './app.module.css';
 import Header from "./components/header/header";
 import Router from "./router/router";
 import Footer from "./components/footer/footer";
 
 const App = ({authService}) => {
-  const [isLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
+  useEffect(() => {
+    authService.authChange(() => {
+      setIsLogin(true);
+    })
+  }, [authService])
 
   return (
     <div className={style.wrapper}>
